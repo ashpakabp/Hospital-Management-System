c4442dbeeaf836e0a491a9cb66b1a076ffd7e3fa
ashpakabp <pathanabp77@gmail.com>
2026-07-21 12:46:42 +0530

    Update Doctor.java

--- Doctor.java
+++ Doctor.java
@@ -1,4 +1,4 @@
-///package com.hospital;
+////package com.hospital;
 public class Doctor {
     private int id; private String name; private String specialization;
     public Doctor(int id,String name,String specialization){
