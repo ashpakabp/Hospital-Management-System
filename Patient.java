03918859efc4c242a02d40cf41df54949f2c44f0
ashpakabp <pathanabp77@gmail.com>
2026-08-08 19:35:02 +0530

    Update Patient.java

--- Patient.java
+++ Patient.java
@@ -1,4 +1,4 @@
-///package com.hospital;
+////package com.hospital;
 public class Patient {
     private int id; private String name; private int age;
     public Patient(int id,String name,int age){this.id=id;this.name=name;this.age=age;}
